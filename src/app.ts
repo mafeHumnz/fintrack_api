1726f0fe7cec8ecc8f3019d202c18fb40121fd3f
mafeHumnz <humanez036@gmail.com>
2026-06-30 16:42:41 -0500

    feat(auth): integrate auth_routes into app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/auth_routes.js";
 
 const app=express();
 
 app.use(
   express.json()
 );
+
+app.use("/auth", authRoutes);
+
 
 export default app;
