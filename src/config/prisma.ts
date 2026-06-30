d770becc8a8d3c4fd265a1c32c1a994a9a61fbb1
mafeHumnz <humanez036@gmail.com>
2026-06-30 17:07:14 -0500

    fix: configure Prisma client with pg adapter

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,9 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env.js";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
