bb863ff5dc3effc2c92514049737b4bf21e6a343
mafeHumnz <humanez036@gmail.com>
2026-07-23 19:24:58 -0500

    feat(transaction): Register transaction routes

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,16 @@ import express from "express";
 import authRoutes from "./routes/auth_routes.js";
 import accountRoutes from "./routes/account_routes.js";
 import categoryRoutes from "./routes/category_routes.js";
+import transactionRoutes from "./routes/transaction_routes.js";
 
-const app=express();
+const app = express();
 
-app.use(
-  express.json()
-);
+app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/accounts", accountRoutes);
 app.use("/categories", categoryRoutes);
+app.use("/transactions", transactionRoutes);
 
 
 export default app;
