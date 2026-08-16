5dab55f6ce51b33cebedd2bbb6158a6cee2e44d1
mafeHumnz <humanez036@gmail.com>
2026-08-16 15:38:00 -0500

    fix: configure ESLint for jest.config.cjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     ignores: ["dist/", "node_modules/", "coverage/"],
+  },
+  {
+    files:["**/*.cjs", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+    sourceType: "commonjs",
+      globals: globals.node,
+    },
   },
   {
     rules: {
